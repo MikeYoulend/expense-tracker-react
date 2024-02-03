f8812a741200d0f1d37cc4b460a330c78674997c
MikeYoulend <mikeyoulend@gmail.com>
2024-02-03 14:42:22 +0100

    To do list - React

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const App = () => {
-	const [newItem, setNewItem] = useState("");
+	const [newItem, setNewItem] = useState(""); //Abbiamo una string vuota
+	const [todos, setTodos] = useState([]);
+
+	function handleSubmit() {
+		e.preventDefault();
+	}
 
 	return (
 		<>
 			{/*Ho usato un fragment per eludere react dal ritornare più componenti e non ho messo il div all'interno per evitare di avere un div inutile */}
-			<form className="new-item-form">
+			<form onSubmit={handleSubmit} className="new-item-form">
 				<div className="form-row">
 					<label htmlFor="item">New Item</label>
 					<input
 						value={newItem}
-						onChange={(e) => setNewItem(e.target.value)}
+						onChange={(e) => setNewItem(e.target.value)} // Questo chiama la funzione setNewItem con il nuovo valore ottenuto dall'elemento di input. In React, setNewItem è una funzione che viene utilizzata per aggiornare lo stato newItem.
 						type="text"
 						id="item"
 					/>
