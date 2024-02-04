8dc94ee91d2de1990235b02d4b32441a84d93c7d
MikeYoulend <mikeyoulend@gmail.com>
2024-02-04 13:40:39 +0100

    Todos Logic

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,7 @@ const App = () => {
 			</form>
 			<h1 className="header">Todo List</h1>
 			<ul className="list">
+				{todos.length === 0 && "No Todos"}
 				{todos.map((todo) => {
 					return (
 						<li key={todo.id}>
